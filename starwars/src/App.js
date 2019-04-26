3230821a85579d91f8cb380e10b00fb682f7e5ff
Jay Maas <jaymaas.dev@gmail.com>
2019-04-26 10:48:48 -0600

    Initial Commit
    README questions have been answered. App MVP functionality has been built.  Moving onto stylings.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Card from './components/Card';
+
 class App extends Component {
   constructor() {
     super();
@@ -12,7 +14,7 @@ class App extends Component {
   componentDidMount() {
     this.getCharacters('https://swapi.co/api/people/');
   }
-
+  
   getCharacters = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
@@ -22,6 +24,7 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
+        console.log(data)
         this.setState({ starwarsChars: data.results });
       })
       .catch(err => {
@@ -30,9 +33,25 @@ class App extends Component {
   };
 
   render() {
+
+    const chars = this.state.starwarsChars
+    let char = chars.map(char => char)
+    char = char.map(character => 
+    <Card 
+    id={character.url}
+    birthYear={character.birth_year} 
+    gender={character.gender} 
+    hairColor={character.hair_color} 
+    height={character.height} 
+    name={character.name} 
+    eye_color={character.eye_color} 
+    key={character.created}
+     />)
+
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        {char}
       </div>
     );
   }
---
+++ starwars/src/components/Card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        console.log()
+        let id = this.props.id
+        let char = this.props.name
+        let eyeColor = this.props.eye_color 
+        let bithYear = this.props.birthYear
+        let gender   = this.props.gender
+        let hairColor = this.props.hairColor 
+        let height = this.props.height
+        return (
+            <div className="card" id={id}>
+           <h1 className="name">Name:{char}</h1>
+           <ul>
+               <li>
+               Eye Color: {eyeColor}
+               </li>
+               <li>
+               Birth Year: {bithYear}
+               </li>
+               <li>
+               Gender: {gender}
+               </li>
+               <li>
+               Hair Color: {hairColor}
+               </li>
+               <li>
+               Height: {height}
+               </li>
+            </ul>
+            </div>
+        )
+    }
+}
+
+export default Card;
